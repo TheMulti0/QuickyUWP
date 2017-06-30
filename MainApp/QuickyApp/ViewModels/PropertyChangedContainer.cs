079dba37207d6f776d3bb8bab534bd2db6ea813e
TheMulti0 <raphael@codeprecise.com>
2017-06-30 12:27:38 +0300

    Tile uncompleted changes

---
+++ MainApp/QuickyApp/ViewModels/PropertyChangedContainer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using QuickyApp.Annotations;
+
+namespace QuickyApp.ViewModels
+{
+    internal class PropertyChangedContainer : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected internal virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
